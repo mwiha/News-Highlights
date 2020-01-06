f2ab234957872af0f3a93abe057ee5e8a4fc5142
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-06 14:11:00 +0300

    created other news sources

--- app/app.py
+++ app/app.py
@@ -5,17 +5,16 @@ from newsapi import NewsApiClient
 app = Flask(__name__)
 
 @app.route('/')
-def Index() :
+def Index():
     newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
-    topheadlines = newsapi.get_top_headlines(sources="cnn")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
     
     articles = topheadlines['articles']
     
     desc = []
     news = []
     img = []
-    
-    
+
     for i in range(len(articles)):
         myarticles = articles[i]
         
@@ -23,12 +22,107 @@ def Index() :
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
         
+    mylist = zip(news,desc,img)
         
-        mylist = zip(news,desc,img)
+    return render_template('index.html', context = mylist)
+
+
+
+@app.route('/')
+def aljazeera ():
+    newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
+    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
+    
+    
+    articles = topheadlines['articles']
+    
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        
+    mylist = zip(news,desc,img)
+        
+    return render_template('aljazeera.html', context = mylist)
+
+
+@app.route('/')
+def abc ():
+    newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
+    
+    
+    articles = topheadlines['articles']
+    
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        
+    mylist = zip(news,desc,img)
+        
+    return render_template('abc.html', context = mylist)
+
         
         
+@app.route('/')
+def ansa ():
+    newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
+    topheadlines = newsapi.get_top_headlines(sources="ansa-news")
+    
+    
+    articles = topheadlines['articles']
+    
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
         
-        return render_template('index.html',context =mylist)
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
         
-if __name__=="__main__":
+    mylist = zip(news,desc,img)
+        
+    return render_template('ansa.html', context = mylist)
+
+
+@app.route('/asso')
+def Associatedpress():
+    newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
+    topheadlines = newsapi.get_top_headlines(sources="associated-press")
+    
+    
+    articles = topheadlines['articles']
+    
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        
+    mylist = zip(news,desc,img)
+        
+    return render_template('asso.html', context = mylist)
+
+if __name__== "__main__":
     app.run(debug=True)

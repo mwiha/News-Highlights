f68cff0fed3735fae14da6d4284f06c1348651cf
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-06 09:17:25 +0300

    connected our application with new config file

--- app/views.py
+++ app/views.py
@@ -8,4 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    
+    title = 'Home - News Highlights'
     return render_template('index.html')

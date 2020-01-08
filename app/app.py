dc271afab9876daf5206eaf594bf241a07a42db4
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-08 09:21:43 +0300

    Deployment to heroku

--- app/app.py
+++ app/app.py
@@ -1,19 +1,44 @@
-from flask import Flask,render_template
-from newsapi import NewsApiClient
+# from flask import Flask,render_template
+# from newsapi import NewsApiClient
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def Index():
+# @app.route('/')
+# def Index():
+# #     newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
+# #     topheadlines = newsapi.get_top_headlines(sources="")
+    
+# #     articles = topheadlines['articles']
+    
+# #     desc = []
+# #     news = []
+# #     img = []
+
+# #     for i in range(len(articles)):
+# #         myarticles = articles[i]
+        
+# #         news.append(myarticles['title'])
+# #         desc.append(myarticles['description'])
+# #         img.append(myarticles['urlToImage'])
+        
+# #     mylist = zip(news,desc,img)
+        
+#     return render_template('index.html')
+
+
+
+# @app.route('/aljazeera')
+# def Aljazeera():
 #     newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
-#     topheadlines = newsapi.get_top_headlines(sources="")
+#     topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
     
 #     articles = topheadlines['articles']
     
 #     desc = []
 #     news = []
 #     img = []
+#     pubAt=[]
 
 #     for i in range(len(articles)):
 #         myarticles = articles[i]
@@ -21,117 +46,92 @@ def Index():
 #         news.append(myarticles['title'])
 #         desc.append(myarticles['description'])
 #         img.append(myarticles['urlToImage'])
+#         pubAt.append(myarticles['publishedAt'])
         
-#     mylist = zip(news,desc,img)
+#     mylist = zip(news,desc,img,pubAt)
         
-    return render_template('index.html')
+#     return render_template('aljazeera.html', context = mylist)
 
 
-
-@app.route('/aljazeera')
-def Aljazeera():
-    newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
-    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
-    
-    articles = topheadlines['articles']
-    
-    desc = []
-    news = []
-    img = []
-    pubAt=[]
-
-    for i in range(len(articles)):
-        myarticles = articles[i]
-        
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-        pubAt.append(myarticles['publishedAt'])
-        
-    mylist = zip(news,desc,img,pubAt)
-        
-    return render_template('aljazeera.html', context = mylist)
-
-
-@app.route('/abc')
-def abc ():
-    newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
-    topheadlines = newsapi.get_top_headlines(sources="abc-news")
+# @app.route('/abc')
+# def abc ():
+#     newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
+#     topheadlines = newsapi.get_top_headlines(sources="abc-news")
     
     
-    articles = topheadlines['articles']
+#     articles = topheadlines['articles']
     
-    desc = []
-    news = []
-    img = []
-    pubAt = []
+#     desc = []
+#     news = []
+#     img = []
+#     pubAt = []
 
-    for i in range(len(articles)):
-        myarticles = articles[i]
+#     for i in range(len(articles)):
+#         myarticles = articles[i]
         
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-        pubAt.append(myarticles['publishedAt'])
+#         news.append(myarticles['title'])
+#         desc.append(myarticles['description'])
+#         img.append(myarticles['urlToImage'])
+#         pubAt.append(myarticles['publishedAt'])
         
-    mylist = zip(news,desc,img, pubAt)
+#     mylist = zip(news,desc,img, pubAt)
         
-    return render_template('abc.html', context = mylist)
+#     return render_template('abc.html', context = mylist)
 
         
         
-@app.route('/cnn')
-def Cnn():
-    newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
-    topheadlines = newsapi.get_top_headlines(sources="cnn")
+# @app.route('/cnn')
+# def Cnn():
+#     newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
+#     topheadlines = newsapi.get_top_headlines(sources="cnn")
     
     
-    articles = topheadlines['articles']
+#     articles = topheadlines['articles']
     
-    desc = []
-    news = []
-    img = []
-    pubAt = []
+#     desc = []
+#     news = []
+#     img = []
+#     pubAt = []
 
-    for i in range(len(articles)):
-        myarticles = articles[i]
+#     for i in range(len(articles)):
+#         myarticles = articles[i]
         
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-        pubAt.append(myarticles['publishedAt'])
+#         news.append(myarticles['title'])
+#         desc.append(myarticles['description'])
+#         img.append(myarticles['urlToImage'])
+#         pubAt.append(myarticles['publishedAt'])
         
-    mylist = zip(news,desc,img,pubAt)
+#     mylist = zip(news,desc,img,pubAt)
         
-    return render_template('cnn.html', context = mylist)
+#     return render_template('cnn.html', context = mylist)
 
 
-@app.route('/asso')
-def Associatedpress():
-    newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
-    topheadlines = newsapi.get_top_headlines(sources="associated-press")
+# @app.route('/asso')
+# def Associatedpress():
+#     newsapi = NewsApiClient(api_key="c2dc80373e954e7fbd0678a357875019")
+#     topheadlines = newsapi.get_top_headlines(sources="associated-press")
     
-    articles = topheadlines['articles']
+#     articles = topheadlines['articles']
     
-    desc = []
-    news = []
-    img = []
-    pubAt= []
+#     desc = []
+#     news = []
+#     img = []
+#     pubAt= []
 
-    for i in range(len(articles)):
-        myarticles = articles[i]
+#     for i in range(len(articles)):
+#         myarticles = articles[i]
         
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-        pubAt.append(myarticles['publishedAt'])
+#         news.append(myarticles['title'])
+#         desc.append(myarticles['description'])
+#         img.append(myarticles['urlToImage'])
+#         pubAt.append(myarticles['publishedAt'])
         
-    mylist = zip(news,desc,img,pubAt)
+#     mylist = zip(news,desc,img,pubAt)
         
-    return render_template('associatedpress.html', context = mylist)
+#     return render_template('associatedpress.html', context = mylist)
 
-if __name__== "__main__":
-    app.run(debug=True) 
+# if __name__== "__main__":
+#     app.run(debug=True) 
     
     
     
